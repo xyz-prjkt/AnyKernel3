859d5b17b27ffc2f8b8de78e2e3d4d2f6bfb7de3
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-11-11 14:38:03 +0700

    AnyKernel3: Custom dtb for lavender
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -35,6 +35,80 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
+# Key select start
+ui_print " "
+ui_print "Touch the screen first or press any volume key"
+
+INSTALLER=$(pwd)
+KEYCHECK=$INSTALLER/tools/keycheck
+chmod 755 $KEYCHECK
+
+keytest() {
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}
+
+choose() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while true; do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
+}
+
+chooseold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $KEYCHECK
+  $KEYCHECK
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 0
+  elif [ $SEL -eq $DOWN ]; then
+    return 1
+  else
+    ui_print "   Vol key not detected!"
+    abort "   Use name change method in TWRP"
+  fi
+}
+
+if [ -z $NEW ]; then
+  if keytest; then
+    FUNCTION=choose
+  else
+    FUNCTION=chooseold
+    ui_print " "
+    ui_print "- Vol Key Programming -"
+    ui_print "   Press Volume Up Key: "
+    $FUNCTION "UP"
+    ui_print "   Press Volume Down Key: "
+    $FUNCTION "DOWN"
+  fi
+  ui_print " "
+  ui_print "- Select Option -"
+  ui_print "   Choose the big CPU clock rate you want:"
+  ui_print "   Volume Up = 1.95 GHz, Volume Down = 2.2 GHz"
+  if $FUNCTION; then
+    NEW=true
+  else
+    NEW=false
+  fi
+else
+  ui_print "   Option specified in zipname!"
+fi
+# Key select end
+
+
 ## AnyKernel install
 dump_boot;
 
@@ -52,7 +126,11 @@ if [ -f $compressed_image ]; then
   fi;
 
   # Concatenate all of the dtbs to the kernel
-  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+  if $NEW; then
+    cat $compressed_image /tmp/anykernel/dtbs/*.dtb-uc > /tmp/anykernel/Image.gz-dtb;
+  else
+    cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+  fi
 fi;
 
 
